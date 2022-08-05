9ad318ab4c11c6a8806c89d71b5bea8b4324d2ea
Gabriel Marquez <gabrielmarquezrivas@gmail.com>
2022-08-05 10:41:53 -0400

    Day 80 - Understanding Swift’s Result type

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -8,24 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
-    // static let tag = "HomeView"
-    @State private var selectedTab = "One"
+    @State private var output = ""
     
     var body: some View {
-        TabView(selection: $selectedTab) {
-            Text("Tab 1")
-                .onTapGesture {
-                    selectedTab = "Two"
-                }
-                .tabItem {
-                    Label("One", systemImage: "star")
-                }
-            Text("Tab 2")
-                .tabItem {
-                    Label("Two", systemImage: "circle")
-                }
-                .tag("Two")
-                // .tag(ContentView.tag)
+        Text(output)
+            .task {
+                await fetchReadings()
+            }
+    }
+    
+    func fetchReadings() async {
+        let fetchTask = Task { () -> String in
+            let url = URL(string: "https://hws.dev/readings.json")!
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let readings = try JSONDecoder().decode([Double].self, from: data)
+            return "Found \(readings.count) readings."
+        }
+        // fetchTask.cancel()
+        let result = await fetchTask.result
+        
+        switch result {
+        case .success(let str):
+            output = str
+        case .failure(let error):
+            output = "Download error: \(error.localizedDescription)"
         }
     }
 }
