c79080032c7588cf34f57d244118b281512da9fa
Gabriel Marquez <gabrielmarquezrivas@gmail.com>
2022-08-08 11:47:17 -0400

    Getting ready for day 82

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -5,43 +5,12 @@
 //  Created by Gabriel Marquez on 2022-08-08.
 //
 
-import UserNotifications
 import SwiftUI
 
 struct ContentView: View {
-    @State private var backgroundColor = Color.red
-
     var body: some View {
-        VStack {
-            Button("Request Permission") {
-                // first
-                UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) {
-                    success, error in
-                    if success {
-                        print("All set!")
-                    } else if let error = error {
-                        print(error.localizedDescription)
-                    }
-                }
-            }
-
-            Button("Schedule Notification") {
-                // second
-                let content = UNMutableNotificationContent()
-                content.title = "Feed the dogs"
-                content.subtitle = "They look hungry"
-                content.sound = UNNotificationSound.default
-
-                // show this notification five seconds from now
-                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
-
-                // choose a random identifier
-                let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
-
-                // add our notification request
-                UNUserNotificationCenter.current().add(request)
-            }
-        }
+        Text("Hello, world!")
+            .padding()
     }
 }
 
