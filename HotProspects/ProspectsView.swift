b9b71407ee8b6414ecdd2a77e44baeb4d2a88637
Gabriel Marquez <gabrielmarquezrivas@gmail.com>
2022-08-09 09:35:57 -0400

    Day 82 - Building the tab bar

---
+++ HotProspects/ProspectsView.swift
@@ -0,0 +1,40 @@
+//
+//  ProspectsView.swift
+//  HotProspects
+//
+//  Created by Gabriel Marquez on 2022-08-09.
+//
+
+import SwiftUI
+
+struct ProspectsView: View {
+    enum FilterType {
+        case none, contacted, uncontacted
+    }
+    
+    let filter: FilterType
+    
+    var body: some View {
+        NavigationView {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                .navigationTitle(title)
+        }
+    }
+    
+    var title: String {
+        switch filter {
+        case .none:
+            return "Everyone"
+        case .contacted:
+            return "Contacted people"
+        case .uncontacted:
+            return "Uncontacted people"
+        }
+    }
+}
+
+struct ProspectsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProspectsView(filter: .none)
+    }
+}
