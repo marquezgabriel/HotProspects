4a13a60ae9fbc5580de769f6555c84fd8df2f2e1
Gabriel Marquez <gabrielmarquezrivas@gmail.com>
2022-08-09 10:06:40 -0400

    Day 82 - Sharing data across tabs using @EnvironmentObject

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject var prospects = Prospects()
+    
     var body: some View {
         TabView {
             ProspectsView(filter: .none)
@@ -27,6 +29,7 @@ struct ContentView: View {
                     Label("Me", systemImage: "person.crop.square")
                 }
         }
+        .environmentObject(prospects)
     }
 }
 
