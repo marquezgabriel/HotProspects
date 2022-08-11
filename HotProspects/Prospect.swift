0ec3075409c61625374970989582d065916928dd
Gabriel Marquez <gabrielmarquezrivas@gmail.com>
2022-08-11 09:21:30 -0400

    Day 84 - Saving and loading data with UserDefaults

--- HotProspects/Prospect.swift
+++ HotProspects/Prospect.swift
@@ -16,13 +16,34 @@ class Prospect: Identifiable, Codable {
 
 @MainActor class Prospects: ObservableObject {
     @Published var people: [Prospect]
+    let saveKey = "SavedData"
 
     init() {
+        if let data = UserDefaults.standard.data(forKey: saveKey) {
+            if let decoded = try? JSONDecoder().decode([Prospect].self, from: data) {
+                people = decoded
+                return
+            }
+        }
+        
+        
         self.people = []
+    }
+    
+    private func save() {
+        if let encoded = try? JSONEncoder().encode(people) {
+            UserDefaults.standard.set(encoded, forKey: saveKey)
+        }
+    }
+    
+    func add(_ prospect: Prospect) {
+        people.append(prospect)
+        save()
     }
     
     func toggle(_ prospect: Prospect) {
         objectWillChange.send()
         prospect.isContacted.toggle()
+        save() 
     }
 }
